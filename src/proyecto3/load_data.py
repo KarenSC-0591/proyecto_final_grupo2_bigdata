3b14ce82fa364303998f40428e29d518c6d23c50
Raul4A <raulcherigo4it@gmail.com>
2026-04-10 11:35:42 -0500

    carga de data empleados proyecto3

---
+++ src/proyecto3/load_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def load_employee_data(file_path):
+
+    """
+
+    Carga el dataset de empleados desde un archivo CSV.
+
+    """
+
+    data = pd.read_csv(file_path)
+
+    print("Datos de empleados cargados correctamente")
+
+    print(data.head())
+
+    return data
+
+
+if __name__ == "__main__":
+
+    df = load_employee_data("data/proyecto3_empleados.csv")
+ 
